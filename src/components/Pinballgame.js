1f2c5b73449b8db2d82f057f3499fa566b12eaa5
Tommy La <tommyl.dmd@gmail.com>
2022-08-02 09:07:30 -0700

    added 7043 games list

--- src/components/Pinballgame.js
+++ src/components/Pinballgame.js
@@ -5,12 +5,14 @@ import image2 from './imgs/1080.png'
 import image3 from './imgs/1306.png'
 import image4 from './imgs/2030.png'
 import image5 from './imgs/2558.png'
+import image6 from './imgs/7043.png'
 
 import pb327 from './pdfs/327pb.pdf'
 import pb1080 from './pdfs/1080pb.pdf'
 import pb1306 from './pdfs/1306pb.pdf'
 import pb2030 from './pdfs/2030pb.pdf'
 import pb2558 from './pdfs/2558pb.pdf'
+import pb7043 from './pdfs/7043pb.pdf'
 
 import heading from './imgs/headings/pinballgamelist.png'
 
@@ -46,6 +48,11 @@ export default function Pinballgame() {
                     <div>
                         <a href={pb2558} target="_blank" rel="noopener noreferrer">
                             <img src={image5} class="w-20 h-20 md:w-56 md:h-56 rounded-lg md:hover:opacity-80" alt="2558games" title="2558 Pinball Games List" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href={pb7043} target="_blank" rel="noopener noreferrer">
+                            <img src={image6} class="w-20 h-20 md:w-56 md:h-56 rounded-lg md:hover:opacity-80" alt="7043games" title="7043 Pinball Games List" />
                         </a>
                     </div>
                 </div>
