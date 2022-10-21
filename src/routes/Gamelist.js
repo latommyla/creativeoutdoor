05d1c7ac0c0e51f84d40cef0451bb6bea539d746
Tommy La <tommyl.dmd@gmail.com>
2022-10-21 16:35:08 -0700

    mobile background changed

--- src/routes/Gamelist.js
+++ src/routes/Gamelist.js
@@ -6,7 +6,7 @@ import Racinggame from '../components/Racinggame';
 
 export default function Gamelist() {
   return (
-    <main class="bg-mobile sm:bg-retro bg-cover bg-fixed bg-center">
+    <main class="bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-rose-400 via-fuchsia-500 to-indigo-500 sm:bg-retro bg-cover bg-fixed bg-center">
       <Arcadegame />
       <Pinballgame />
       <Racinggame />
