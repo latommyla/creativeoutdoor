c3acaf8686365972ed6ad5b8c50b051349d6251c
Tommy La <tommyl.dmd@gmail.com>
2022-10-21 10:34:52 -0700

    fixed mobile bg image location

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {},
     backgroundImage: {
       'retro': "url('/src/components/backgroundimg/retro-wave.png')",
-      'mobile': "url('/Users/ecom/creativeoutdoor/src/components/backgroundimg/mobile-bg.png')",
+      'mobile': "url('/src/components/backgroundimg/mobile-bg.png')",
     },
     fontFamily: {
       'silkscreen': ['"Silkscreen"', 'cursive'],
